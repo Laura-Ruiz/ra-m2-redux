3509f8cff74ce2b9d512cf937aa2ef0641afd07e
Laura-Ruiz <laaura.26@gmail.com>
2023-01-10 18:33:25 +0100

    first commit

--- src/store/houses.slice.js
+++ src/store/houses.slice.js
@@ -13,9 +13,9 @@ const initialState = {
 
 export const getHouses = createAsyncThunk(
   'houses/getHouses',
-  async () => {
-  // const url = `${urls.houses}?page=${currentPage}&_limit=${9*currentPage}`
-  const response = await fetch(urls.houses)
+  async (currentPage) => {
+  const url = `${urls.houses}?page=${currentPage}&_limit=${9*currentPage}`
+  const response = await fetch(url)
   const data = await response.json()
    return data
 } 
